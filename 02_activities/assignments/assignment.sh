57ae2d2c28eebdc82c96feae5cc7f8f13575931d
brtrivedi5 <Brijtriv10@gmail.com>
2024-11-22 11:19:47 -0500

    learnig more and updating my work, its not completed yet

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -23,7 +23,6 @@ unzip rawdata.zip
 # 1. Create a directory named data
 mkdir data
 # 2. Move the ./rawdata directory to ./data/raw
-mkdir data/raw
 mv ./rawdata ./data/raw
 # 3. List the contents of the ./data/raw directory
 ls ./data/raw
@@ -31,10 +30,10 @@ ls ./data/raw
 mkdir data/processed
 mkdir data/processed/server_logs data/processed/user_logs data/processed/event_logs
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
-find ./data/raw -type f -name '*server*.log' -exec cp {} ./data/processed/server_logs/
+cp ./data/raw/rawdata/*server*log ./data/processed/server_logs
 # 6. Repeat the above step for user logs and event logs
-cp ./data/raw/rawdata/*user*.log ./data/processed/user_logs
-cp ./data/raw/rawdata/*event*.log ./data/processed/event_logs
+cp ./data/raw/rawdata/*user*log ./data/processed/user_logs
+cp ./data/raw/rawdata/*event*log ./data/processed/event_logs
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
 rm ./data/raw/*ipaddr*
 rm ./data/processed/*ipaddr*
